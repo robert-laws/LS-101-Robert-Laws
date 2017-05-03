841d60052ab0f5255c037a189c0d9d80e8780276
Robert Laws <roblaws@me.com>
2017-05-03 15:04:44 +0300

    lesson 3 - easy 1 - begin

--- lesson_3/easy_1.rb
+++ lesson_3/easy_1.rb
@@ -1,1 +1,41 @@
 # Easy 1
+
+# Question 1
+# numbers = [1,2,2,3]
+# numbers.uniq
+#
+# p numbers
+#
+# # Question 2
+# name = "kal"
+# age = 42
+# result = false
+#
+# p name != age
+# p !name
+# p numbers.uniq!
+# p result ? name : age
+# p name.split(//).include?("a")
+# p !!name
+# p !!result
+
+
+# advice = "Few things in life are as important as house training your pet dinosaur."
+# advice.gsub!("important", "urgent")
+# p advice
+#
+#
+# numbers = [1, 2, 3, 4, 5]
+# numbers.delete_at(1)
+# numbers.delete(1)
+# p numbers
+#
+# num = 9
+# p (10..100).cover?(num)
+
+famous_words = "seven years ago..."
+famous_words.insert(0, "Four score and ")
+p famous_words
+
+famous_words.prepend("Four score and ")
+p famous_words
