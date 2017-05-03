0b9503cb9a2f2d817a9022e7d9c435e3cead30b4
Robert Laws <roblaws@me.com>
2017-05-03 09:51:39 +0300

    file to help with quiz for lesson 2

---
+++ quiz/quiz.rb
@@ -0,0 +1,45 @@
+# puts "Say something..."
+# response = Kernel.gets().chomp()
+#
+# puts response
+
+
+# sentence = "hello to all the wonderful people here"
+#
+# def longest_word(sentence)
+#   words = sentence.split
+#   saved_word = words.shift
+#
+#   words.each do |word|
+#     if saved_word.length < word.length
+#       saved_word = word
+#     end
+#   end
+#
+#   saved_word
+# end
+#
+# p longest_word(sentence)
+
+# name = ["bob", "jim"]
+#
+# def get_name(n)
+#   n << "hal"
+# end
+#
+# p name
+# p get_name(name)
+# p name
+
+def name
+  "George"
+end
+
+name = "Lisa"
+
+
+
+loop do
+  puts name
+  break
+end
